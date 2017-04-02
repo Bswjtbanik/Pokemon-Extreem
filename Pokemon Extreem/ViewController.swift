3f20ea2ff8841840744e2e3a7a9bdf058947b8a8
Biswajit banik <bswjtbanik@gmail.com>
2017-04-02 21:30:04 +0600

    Map view

--- Pokemon Extreem/ViewController.swift
+++ Pokemon Extreem/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MKMapViewDelegate , CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        manager.delegate = self
+        
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            
+            print("Ready to go ...")
+            mapView.showsUserLocation = true
+            manager.stopUpdatingLocation()
+            
+        }else{
+        manager.requestWhenInUseAuthorization()
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let region = MKCoordinateRegionMakeWithDistance((manager.location!.coordinate), 100 , 100)
+        mapView.setRegion(region, animated: true)
+        
+        
     }
-
+    
 
 }
 
